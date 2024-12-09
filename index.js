b22d258852fca0997d92bb017092e767c4a65294
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-12-09 19:20:06 -0300

    as

---
+++ index.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    // Inicia un navegador
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Navega a la página deseada
+    await page.goto('https://store.steampowered.com/search/?os=win&filter=topsellers&ndl=1');
+
+    // Extrae el contenido del elemento
+    //search_resultsRows
+
+    const data = await page.evaluate(() => {
+        
+        const juegos = document.getElementById('search_resultsRows');
+       
+                
+
+
+        for(i = 0; i < 20; i++){
+            
+        }
+        const title = document.querySelector('h1').innerText;
+        const description = document.querySelector('p').innerText;
+
+
+
+        return { title, description };
+    });
+
+    console.log(data);
+
+    // Cierra el navegador
+    await browser.close();
+})();
